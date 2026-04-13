8b7aedd75cdc5d0ade0b19c7c0a9b187a3c1dc09
nodebug <nodebug@outlook.com>
2026-04-13 18:00:01 +0200

    add typescript support (#4)

--- index.js
+++ index.js
@@ -2,21 +2,39 @@ import { createWriteStream } from 'fs'
 import gifenc from 'gifenc'
 import sharp from 'sharp'
 
+/**
+ * GIF encoder class for creating animated GIFs from images
+ */
 class Gif {
-  get width() {
-    return this._width
-  }
-
-  get height() {
-    return this._height
-  }
-
+  /**
+   * @param {number} width - The width of the GIF
+   * @param {number} height - The height of the GIF
+   */
   constructor(width, height) {
     this._width = width
     this._height = height
     this._frames = []
   }
 
+  /**
+   * @returns {number} The width of the GIF
+   */
+  get width() {
+    return this._width
+  }
+
+  /**
+   * @returns {number} The height of the GIF
+   */
+  get height() {
+    return this._height
+  }
+
+  /**
+   * Add an image from a file path to the GIF
+   * @param {string} path - Path to the image file
+   * @returns {Promise<boolean>} Promise that resolves to true when complete
+   */
   async addImage(path) {
     // Read and process the image with sharp
     const imageBuffer = await sharp(path)
@@ -29,6 +47,11 @@ class Gif {
     return true
   }
 
+  /**
+   * Add an image from a base64 buffer to the GIF
+   * @param {string} data - Base64 encoded image data
+   * @returns {Promise<boolean>} Promise that resolves to true when complete
+   */
   async addBuffer(data) {
     // Process base64 buffer with sharp
     const imageBuffer = await sharp(Buffer.from(data, 'base64'))
@@ -41,6 +64,11 @@ class Gif {
     return true
   }
 
+  /**
+   * Add multiple images from file paths to the GIF
+   * @param {string[]} paths - Array of image file paths
+   * @returns {Promise<PromiseSettledResult<boolean>[]>} Promise that resolves to array of results
+   */
   async addImages(paths) {
     const promises = paths.map(async (path) => {
       await this.addImage(path)
@@ -48,6 +76,11 @@ class Gif {
     return Promise.all(promises)
   }
 
+  /**
+   * Save the GIF to a file or return as buffer
+   * @param {string} [path] - Optional file path to save to. If not provided, returns buffer
+   * @returns {Promise<Buffer|void>} Promise that resolves to buffer when no path provided, or void when path provided
+   */
   async save(path) {
     if (this._frames.length === 0) {
       throw new Error('No frames to save')
